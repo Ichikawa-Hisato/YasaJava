9be7ba2b7b7c8ad6b03980b4cbe72bb41b514784
ichikawa hisato <hi344421.lemon@gmail.com>
2019-07-02 17:52:03 +0900

    XML&NetWork
    20190702_AddNew

---
+++ YasaJava/YIHLesson10/Sample1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+import javax.xml.transform.dom.*;
+import org.w3c.dom.*;
+
+public class Sample1{
+    public static void main(String[] args)throws Exception{
+        //DOMの準備をする
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+
+        //文章を読み込む
+        Document doc = db.parse(new FileInputStream("Sample.xml"));
+
+        //文章を書き出す
+        TransformerFactory tff = TransformerFactory.newInstance();
+        Transformer tf = tff.newTransformer();
+        
+        tf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        tf.transform(new DOMSource(doc), new StreamResult("result.xml"));
+        System.out.println("result.xmlに出力しました。");
+    }
+}
